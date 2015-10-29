1a70054dea5aba90ff2af1105e69eaf73624827e
proudzhu <proudzhu.fdu@gmail.com>
2015-10-29 21:28:42 +0800

    add UglyNumber2

---
+++ include/UglyNumber2.hpp
@@ -0,0 +1,6 @@
+#ifndef _UGLYNUMBER2_HPP
+#define _UGLYNUMBER2_HPP
+
+int nthUglyNumber(int n);
+
+#endif
---
+++ tests/UglyNumber2Test.cpp
@@ -0,0 +1,16 @@
+#include "Catch/single_include/catch.hpp"
+
+#include "UglyNumber2.hpp"
+
+TEST_CASE("UglyNumber2") {
+	REQUIRE(nthUglyNumber(1) == 1);
+	REQUIRE(nthUglyNumber(2) == 2);
+	REQUIRE(nthUglyNumber(3) == 3);
+	REQUIRE(nthUglyNumber(4) == 4);
+	REQUIRE(nthUglyNumber(5) == 5);
+	REQUIRE(nthUglyNumber(6) == 6);
+	REQUIRE(nthUglyNumber(7) == 8);
+	REQUIRE(nthUglyNumber(8) == 9);
+	REQUIRE(nthUglyNumber(9) == 10);
+	REQUIRE(nthUglyNumber(10) == 12);
+}
