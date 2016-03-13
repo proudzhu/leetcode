e566f5ccc719647728af94fc58be82d61b5b07a7
proudzhu <proudzhu.fdu@gmail.com>
2016-03-13 19:16:35 +0800

    ad TreeNode.hpp

---
+++ include/TreeNode.hpp
@@ -0,0 +1,13 @@
+#ifndef _TREENODE_HPP
+#define _TREENODE_HPP
+
+struct TreeNode {
+	int val;
+	TreeNode *left;
+	TreeNode *right;
+	TreeNode(int x):val(x), left(NULL), right(NULL) {}
+};
+
+TreeNode* createTree();
+
+#endif
