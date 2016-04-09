cf8bb34be1fa06e3b8d1e249722dc17d043fcb56
proudzhu <proudzhu.fdu@gmail.com>
2016-04-09 20:02:06 +0800

    add power of two

---
+++ include/PowerOfTwo.hpp
@@ -0,0 +1,6 @@
+#ifndef _POWER_OF_TWO_HPP
+#define _POWER_OF_TWO_HPP
+
+bool isPowerOfTwo(int n);
+
+#endif
---
+++ tests/PowerOfTwoTest.cpp
@@ -0,0 +1,14 @@
+#include "Catch/single_include/catch.hpp"
+
+#include "PowerOfTwo.hpp"
+
+TEST_CASE("PowerOfTwo") {
+	REQUIRE(isPowerOfTwo(1) == 1);
+	REQUIRE(isPowerOfTwo(2) == 1);
+	REQUIRE(isPowerOfTwo(3) == 0);
+	REQUIRE(isPowerOfTwo(4) == 1);
+	REQUIRE(isPowerOfTwo(8) == 1);
+	REQUIRE(isPowerOfTwo(16) == 1);
+	REQUIRE(isPowerOfTwo(32) == 1);
+	REQUIRE(isPowerOfTwo(65536) == 1);
+}
